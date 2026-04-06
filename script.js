637b1f9b254e154af152bcbffe9375709c9c5492
Gamer902GT <gamer902gt@gmail.com>
2026-04-05 22:36:10 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,53 @@
-let categoriaActual = "todos";
+let carrito = [];
 
-const buscador = document.getElementById("buscador");
-
-buscador.addEventListener("keyup", aplicarFiltros);
-
-function filtrar(categoria) {
-  categoriaActual = categoria;
-
-  document.querySelectorAll(".filtros button").forEach(btn => {
-    btn.classList.remove("activo");
-  });
-
-  event.target.classList.add("activo");
-
-  aplicarFiltros();
+function agregarCarrito(nombre, precio) {
+  carrito.push({ nombre, precio });
+  actualizarCarrito();
 }
 
-function aplicarFiltros() {
-  let texto = buscador.value.toLowerCase();
-  let productos = document.querySelectorAll(".card");
+function actualizarCarrito() {
+  let lista = document.getElementById("lista-carrito");
+  let total = document.getElementById("total");
 
-  productos.forEach(p => {
-    let coincideTexto = p.innerText.toLowerCase().includes(texto);
-    let coincideCategoria = categoriaActual === "todos" || p.classList.contains(categoriaActual);
+  lista.innerHTML = "";
+  let suma = 0;
 
-    if (coincideTexto && coincideCategoria) {
-      p.style.display = "block";
-    } else {
-      p.style.display = "none";
-    }
+  carrito.forEach((producto, index) => {
+    suma += producto.precio;
+
+    lista.innerHTML += `
+      <li>
+        ${producto.nombre} - $${producto.precio}
+        <button onclick="eliminar(${index})">❌</button>
+      </li>
+    `;
   });
+
+  total.innerText = "Total: $" + suma;
+}
+
+function eliminar(index) {
+  carrito.splice(index, 1);
+  actualizarCarrito();
+}
+
+function enviarWhatsApp() {
+  if (carrito.length === 0) {
+    alert("El carrito está vacío");
+    return;
+  }
+
+  let mensaje = "Hola, quiero pedir:\n";
+
+  carrito.forEach(p => {
+    mensaje += `- ${p.nombre} ($${p.precio})\n`;
+  });
+
+  let total = carrito.reduce((acc, p) => acc + p.precio, 0);
+
+  mensaje += `Total: $${total}`;
+
+  let url = "https://wa.me/573218299283?text=" + encodeURIComponent(mensaje);
+
+  window.open(url, "_blank");
 }
